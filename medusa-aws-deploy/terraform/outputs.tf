ab2f496b680735ae939ddd7e105e43292d810f5f
BalagouniNaveen <162671532+BalagouniNaveen@users.noreply.github.com>
2025-07-13 20:21:36 +0530

    Update outputs.tf

--- medusa-aws-deploy/terraform/outputs.tf
+++ medusa-aws-deploy/terraform/outputs.tf
@@ -1,1 +1,11 @@
-terraform
+output "alb_dns_name" {
+  value = aws_lb.medusa_alb.dns_name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
+
+output "redis_endpoint" {
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
+}
