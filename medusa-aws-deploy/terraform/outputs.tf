35a5c22676068cac565181d0101dd55b88cf2aa2
BalagouniNaveen <162671532+BalagouniNaveen@users.noreply.github.com>
2025-07-16 09:19:35 +0530

    Update outputs.tf

--- medusa-aws-deploy/terraform/outputs.tf
+++ medusa-aws-deploy/terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "alb_dns_name" {
+output "alb_dns_name" {                      # Exposes the DNS name of the Application Load Balancer
   value = aws_lb.medusa_alb.dns_name
 }
 
-output "rds_endpoint" {
+output "rds_endpoint" {                      # Outputs the connection endpoint (hostname) for the RDS instance
   value = aws_db_instance.postgres.endpoint
 }
 
-output "redis_endpoint" {
+output "redis_endpoint" {                    # Provides the Redis cluster’s node address for use in applications
   value = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
